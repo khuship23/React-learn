9c2c40eb38a1a4baea5dc59f860539264121695d
Khushi Pardeshi <pardeshikhushi23@gmail.com>
2024-06-15 12:10:05 +0530

    basics

--- app.js
+++
@@ -1,6 +0,0 @@
-
-      const heading1 = React.createElement("h1", {id : "heading"}, "hello from react");
-      const root1 = ReactDOM.createRoot(document.getElementById("root"));
-      root1.render(heading1);
-
-
--- lec 1/app.js
+++ lec 1/app.js
@@ -1,6 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+     
+// react createElements => object => htmlElement(render)
 
-      const heading1 = React.createElement("h1", {id : "heading"}, "hello from react");
-      const root1 = ReactDOM.createRoot(document.getElementById("root"));
-      root1.render(heading1);
+// const heading = React.createElement("h1", {id: "heading"}, "Namaste react");
 
+const jsxheading = <h1 className="head "> Namaste React by JSX</h1>;
 
+// react functional component
+//function that return a jsx code
+const no = 100;
+const HeadingComponent = () => {
+      return <h1>Namaste React by Functional Component</h1>;
+}
+const HeadingComponent1 = () => (
+      <div>
+           <h2>{no}</h2> 
+            <HeadingComponent />
+            <HeadingComponent></HeadingComponent>
+            {HeadingComponents()}
+       <h1 className="head">Namaste React Functional Component</h1>
+       </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(jsxheading);// replaced 
+root.render(<HeadingComponent1 />);
