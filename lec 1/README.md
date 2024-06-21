106fa6d63178b81e738be118a766088ba14543ee
Khushi Pardeshi <pardeshikhushi23@gmail.com>
2024-06-21 22:36:46 +0530

    update

--- lec 1/README.md
+++ lec 1/README.md
@@ -1,1 +1,15 @@
 React 
+
+# React HOOKS
+normal js functions
+- useState() -state variables
+- useEffect()
+whenever a state variable updates react rerenders the state
+data layer in sync with ui layer
+reconciliation algorithm (react fiber) react-16
+
+VIRTUAL DOM
+- representation of actual DOM
+
+Diff Algorithm 
+difference between two virtual DOMs
--- lec 1/src/components/Body.js
+++ lec 1/src/components/Body.js
@@ -1,19 +1,114 @@
 import RestaurantCard from "./RestaurantCard";
 import resObj from "../../utils/mockData";
+import {useState} from "react";
 const Body =() =>{
+  let [listOfRestaurants, setListOfRestaurants] =useState(resObj);
+
+  //  let  [listOfRestaurants, setListOfRestaurants] =useState(
+  //   [
+  //   {
+  //         "info": {
+  //           "id": "527591",
+  //           "name": "Wow! Momo",
+  //           "cloudinaryImageId": "64fd45fd9f44c1737bc446e470bed666",
+  //           "costForTwo": "₹300 for two",
+  //           "cuisines": [
+  //             "Tibetan",
+  //             "Healthy Food",
+  //             "Asian",
+  //             "Chinese",
+  //             "Snacks",
+  //             "Continental",
+  //             "Desserts",
+  //             "Beverages"
+  //           ],
+  //           "avgRating": 4.2,
+  //           "sla": {
+  //             "deliveryTime": 36
+  //           }
+  //         }
+  //       }, {"info": {
+  //             "id": "527592",
+  //             "name": " Momo",
+  //             "cloudinaryImageId": "64fd45fd9f44c1737bc446e470bed666",
+  //             "costForTwo": "₹300 for two",
+  //             "cuisines": [
+  //               "Tibetan",
+  //               "Healthy Food",
+  //               "Asian",
+  //               "Chinese",
+  //               "Snacks",
+  //               "Continental",
+  //               "Desserts",
+  //               "Beverages"
+  //             ],
+  //             "avgRating": 3,
+  //             "sla": {
+  //               "deliveryTime": 36
+  //             }}}
+  //  ]);
+
+    // let listOfRestaurants = [{
+    //     "info": {
+    //       "id": "527591",
+    //       "name": "Wow! Momo",
+    //       "cloudinaryImageId": "64fd45fd9f44c1737bc446e470bed666",
+    //       "costForTwo": "₹300 for two",
+    //       "cuisines": [
+    //         "Tibetan",
+    //         "Healthy Food",
+    //         "Asian",
+    //         "Chinese",
+    //         "Snacks",
+    //         "Continental",
+    //         "Desserts",
+    //         "Beverages"
+    //       ],
+    //       "avgRating": 4.2,
+    //       "sla": {
+    //         "deliveryTime": 36
+    //       }
+    //     }
+    //   },  {"info": {
+    //     "id": "527592",
+    //     "name": " Momo",
+    //     "cloudinaryImageId": "64fd45fd9f44c1737bc446e470bed666",
+    //     "costForTwo": "₹300 for two",
+    //     "cuisines": [
+    //       "Tibetan",
+    //       "Healthy Food",
+    //       "Asian",
+    //       "Chinese",
+    //       "Snacks",
+    //       "Continental",
+    //       "Desserts",
+    //       "Beverages"
+    //     ],
+    //     "avgRating": 3,
+    //     "sla": {
+    //       "deliveryTime": 36
+    //     }}
+    //   }];
 
     return(
         <div className="body">
             <div className="filter">
                 <button className="filter-btn" 
-                onClick={()=>{}}>
+                onClick={()=>{
+                  
+                 const filteredList = listOfRestaurants.filter((info) =>{
+                    return info.info.avgRating > 4.5
+                  })
+                  console.log(listOfRestaurants);
+                  setListOfRestaurants(filteredList);
+                }}>
                 Top Rate Restaurants
                 </button>
             </div>
             <div className="res-container">
                 {/* passing props to a functional components  */}
               {
-                resObj.map((info )=> (
+                listOfRestaurants.map((info )=> (
                   <RestaurantCard key={info.info.id} resData={info}/>
                   ))
 
