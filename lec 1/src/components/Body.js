12ed2448770144170744624a6b23bbf8cab900f7
Khushi Pardeshi <pardeshikhushi23@gmail.com>
2024-06-23 23:53:18 +0530

    useEffect

--- lec 1/src/components/Body.js
+++ lec 1/src/components/Body.js
@@ -6,7 +6,17 @@ const Body =() =>{
  
   useEffect(()=>{
     console.log("useeffect called");
+    fetchData();
   }, []); 
+
+  const fetchData = async() =>{
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+   
+const json = await data.json();
+console.log(json);
+  }
 
     return(
         <div className="body">
