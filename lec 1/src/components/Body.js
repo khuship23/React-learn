a1f5bf149d062430106814c0287efeb74e00076e
Khushi Pardeshi <pardeshikhushi23@gmail.com>
2024-07-03 01:51:16 +0530

    conditional rendering

--- lec 1/src/components/Body.js
+++ lec 1/src/components/Body.js
@@ -20,11 +20,13 @@ const json = await data.json();
 console.log(json);
 setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants )
   }
-  if (listOfRestaurants.length === 0){
-    return <Shimmer/>;
-  }
 
-    return(
+  //conditional rendering
+  // if (listOfRestaurants.length === 0){
+  //   return <Shimmer/>;
+  // }
+
+    return listOfRestaurants.length === 0 ? <Shimmer/> :(
         <div className="body">
             <div className="filter">
                 <button className="filter-btn" 
