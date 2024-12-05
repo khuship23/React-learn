37bbd5b372a75b679500056ce48e024c1e047ab0
khuship23 <pardeshikhushi23@gmail.com>
2024-12-06 01:05:29 +0530

    class component

--- lec 1/src/components/User.js
+++ lec 1/src/components/User.js
@@ -1,7 +1,19 @@
-const User = ({name}) => {
+import { useEffect } from "react";
+
+const User = ({ name }) => {
+  // useEffect(() => {
+  //   const timer =setInterval(() => {
+  //     console.log("hello")
+  //   }, 10000);
+
+  //   //unmounting = > when we leave that page and component is removed
+  //   return () => {
+  //     clearInterval(timer);
+  //   }
+  // })
   return (
     <div className="myuser">
-          <h2 >Name: {name}</h2>
+      <h2>Name: {name}</h2>
       <h3>Location: Pune</h3>
       <h4>Contant: @khushiness</h4>
     </div>
--- lec 1/src/components/UserClass.js
+++ lec 1/src/components/UserClass.js
@@ -3,27 +3,54 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
-      count2: 4,
+      userInfo: {
+        name: "dummy name",
+        location: "default",
+        // avatar_url: "https://dummy-photo.com",
+      },
     };
     // state variable earlier no state instance of the class should create a stat
   }
-  componentDidMount() {}
+  async componentDidMount() {
+    console.log("component did mount");
+    // lifecycle => constructor, render, component did mount(only when component is mounted)
+    // use to make api call but why because only component is render when only we make the api
+    // as we dont want to wait for api to render the data
+    //
+    const data = await fetch("https://api.github.com/users/khuship23");
+    const json = await data.json();
+    this.setState({
+      userInfo: json,
+    });
+    console.log(json);
+  }
+  componentDidUpdate() {
+    console.log("component did update");
+  }
+  componentWillUnmount() {
+    //when components will be removed
+    // we have return  function in useeffect for this
+  }
   render() {
     // const {name, location} = this.props
-    const { count, count2 } = this.state;
+    const { name, login, html_url, avatar_url } = this.state.userInfo;
     return (
       <div className="myuser">
-        <h2>Name: {count}</h2>
-        <button
+        <h2>Name: {name}</h2>
+        <img src={avatar_url} />
+        {/* <button
           onClick={() => {
-            this.setState({ count: this.state.count + 1 });
+            // never update state variable directly
+            // we make use of setstate function to update state
+            this.setState({
+              count: this.state.count + 1,
+            });
           }}
         >
           Count Increase
-        </button>
-        <h3>Location: Pune</h3>
-        <h4>Contant: @khushiness</h4>
+        </button> */}
+        <h3>Location:{html_url}</h3>
+        <h4>Contact: {login}</h4>
       </div>
     );
   }
